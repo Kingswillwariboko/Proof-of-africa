f1162877f05d10248632745be60e01adda32e8a1
Kingswill Wariboko <kingswillwariboko@gmail.com>
2023-02-20 20:33:20 +0100

    changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import logo from "../../assets/Proof of Africa logo con outline e tagline 2.svg"
+import menu from "../../assets/menu-01.svg"
+
+
+import "./header.scss"
+
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="header__logo">
+                <img src={logo} alt=""/>
+            </div>
+
+            <div className="header__menu">
+                <img src={menu} alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
