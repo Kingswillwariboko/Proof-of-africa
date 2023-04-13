96de7cf6c6a8b2191cca392993d48d7f0f11baa9
Kingswill Wariboko <kingswillwariboko@gmail.com>
2023-04-13 17:57:07 +0100

    another changes

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -12,44 +12,53 @@ const datas =[
     {
       first : {
             title:"Part 1: A Look into History",
-            description: "The Kikuyu also known as the 'Gikuyu' people ar the first Bantu ethnic group native to Central Kenya, who undertook the anti-colonial agitation in the 1920s. They believe in..."
+            description: "The Kikuyu also known as the 'Gikuyu' people ar the first Bantu ethnic group native to Central Kenya, who undertook the anti-colonial agitation in the 1920s. They believe in...",
+            link: "https://twitter.com/poapool/status/1584908056709808130?t=3EupSz5Z0yY_YeZQvWGbIg&s=19"
         },
       second:   {
             title:"Part 2: Way of Life",
-            description: "The Mugumo (Fig tree) is a sacred tree among the Kikuyu people and is key to understanding the cosmology of the.."
+            description: "The Mugumo (Fig tree) is a sacred tree among the Kikuyu people and is key to understanding the cosmology of the..",
+            link: "https://twitter.com/poapool/status/1585648516948336642?t=ByGxt3ZA1VZD0PkwQac6yw&s=19"
         },
       third:  {
             title:"Part 3: Music",
-            description: "The Kikuyu were originally agriculturalists, however, after their harvests, they participated in choral dances coupled with..."
+            description: "The Kikuyu were originally agriculturalists, however, after their harvests, they participated in choral dances coupled with...",
+            link: "https://twitter.com/poapool/status/1587477215712886792?t=lxitcCaFdSgTNRr9Ho5iPA&s=19"
         },
     },
     {
         first : {
               title:"Part 1: A Look into History",
-              description: "The Maasai also known as the 'maa' people were a monotheistic dominating tribe at the beginning of 20th century. They are one of the very few..."
+              description: "The Maasai also known as the 'maa' people were a monotheistic dominating tribe at the beginning of 20th century. They are one of the very few...",
+              link: "https://twitter.com/poapool/status/1580575607465078784?t=hzWW-YW9lrFiK9qoJoCfrQ&s=19"
           },
         second:   {
               title:"Part 2: Way of Life",
-              description: "Through a Child's eyes…"
+              description: "Through a Child's eyes…",
+              link: "https://twitter.com/poapool/status/1582386546954551296?t=nz-5guEudbsEeQbu0kOeXg&s=19"
           },
         third:  {
               title:"Part 3: Music",
-              description: "The Maa people don't use instruments when singing or dancing. All of their music is vocal and comprises rhythms rendered by..."
+              description: "The Maa people don't use instruments when singing or dancing. All of their music is vocal and comprises rhythms rendered by...",
+              link: "https://twitter.com/poapool/status/1583096359141355520?t=pvKtoYGhzT0P1WQIGUrDKg&s=19"
           },
       },
 
       {
         first : {
               title:"Part 1: A Look into History",
-              description: "The Igbo people (Ndi Igbo) are an ethnic group known for their metalworking art that flourished as early as the ninth century. Theocratic in nature, spirituality played a…"
+              description: "The Igbo people (Ndi Igbo) are an ethnic group known for their metalworking art that flourished as early as the ninth century. Theocratic in nature, spirituality played a…",
+              link: "https://twitter.com/poapool/status/1590745977220370432?s=20&t=hEpB5dSObxX1pc6wQuKTgQ"
           },
         second:   {
               title:"Part 2: Way of Life",
-              description: "The oldest Igbo state known as the Kingdom of Nri dates back to 900 AD, and had the most popular political and religious influence over…"
+              description: "The oldest Igbo state known as the Kingdom of Nri dates back to 900 AD, and had the most popular political and religious influence over…",
+              link: "https://twitter.com/poapool/status/1592548605474537472?s=20&t=2RCPEoYLXhdshtR28Sx_5Q"
           },
         third:  {
               title:"Part 3: Music",
-              description: "Traditional music and dance are indispensable parts of the Igbo culture. They serve as a mark of identity and a unifying factor that binds the people together. Ogene is a style of Igbo music that…"
+              description: "Traditional music and dance are indispensable parts of the Igbo culture. They serve as a mark of identity and a unifying factor that binds the people together. Ogene is a style of Igbo music that…",
+              link: "https://twitter.com/poapool/status/1593277602789314561?s=20&t=2RCPEoYLXhdshtR28Sx_5Q"
           },
       }
  
@@ -119,30 +128,35 @@ const Carousel = () => {
                             <div  className='slider__main-one'>
                             <h5>{data.first.title}</h5>
                             <p>{data.first.description}</p>
-
+                            <a href={data.first.link}>
                             <button className='bt'>
                                     <p>Read more</p>
                                     <img src={arrow} alt=""/>
                             </button>
+                            </a>
                         </div>
 
                         <div  className='slider__main-one'>
                             <h5>{data.second.title}</h5>
                             <p>{data.second.description}</p>
 
+                            <a href={data.second.link}>
                             <button className='bt'>
                                     <p>Read more</p>
                                     <img src={arrow} alt=""/>
                             </button>
+                            </a>
                         </div>
                         <div className='slider__main-one'>
                             <h5>{data.third.title}</h5>
                             <p>{data.third.description}</p>
 
+                            <a href={data.third.link}>
                             <button className='bt'>
                                     <p>Read more</p>
                                     <img src={arrow} alt=""/>
                             </button>
+                            </a>
                         </div>
                        
                         </div>
